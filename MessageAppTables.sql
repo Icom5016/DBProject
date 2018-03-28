4f26e0a2bc6acf3a307333b461c060a0d4777abc
Irixa Vales <irixa.vales@upr.edu>
2018-03-28 19:18:05 -0400

    Updated SQL tables

--- MessageAppTables.sql
+++ MessageAppTables.sql
@@ -1,61 +1,62 @@
 CREATE TABLE Dashboard (
-	sid int serial primary key,
-  date date,
-  total_messages int,
-  total_replies int,
-  total_likes int,
-  total_dislikes int,
-  total_active_users int);
+	dash_id int serial primary key, --dashboard id
+  date date, --dashboard date
+  total_messages int, --total messages in day
+  total_replies int, --total replies in day
+  total_likes int, --total likes in day
+  total_dislikes int, --total dislikes in day
+  total_active_users int); --total active users in day
 
 CREATE TABLE Hashtag(
-	hid int serial primary key, 
-	text string, 
-	frequency int);
+	hash_id int serial primary key, --hashtag id
+	text string, --hashtag text
+	frequency int); --hashtag frequency
 
 CREATE TABLE Message(
-	mid int serial primary key, 
-	text string, 
-	likes int, 
-	dislikes int, 
-	sentby int references User(uid), 
-	cgid int references Group_Chat(gcid),
-	date string);
+	msgid int serial primary key, --message id
+	text string, --message text
+	likes int, --message likes
+	dislikes int, --message dislikes
+	date date, --message date
+	time time, --message time
+	user_id int references User(uid), --user that sent the message
+	gchat_id int references Group_Chat(gcid)); --group chat where it was sent
 
 CREATE TABLE Group_Chat(
-	gcid int serial primary key, 
-	gcname varchar(12), 
-	gcownerid int references User(uid));
+	gchat_id int serial primary key, --group chat id
+	gchat_name varchar(12), --group chat name
+	user_id int references User(uid)); --owner of group chat
 
 CREATE TABLE User(
-	uid int serial primary key, 
-	firstname varchar(50),
-	lastname varchar(50),
-	email string, 
-	phone char(10),
-	password varchar(50));
+	user_id int serial primary key, --user id
+	first_name varchar(50), --user first name
+	last_name varchar(50), --user last name
+	email unique string, --user email
+	phone unique char(10), --user phone
+	username unique varchar(15), --user username
+	password varchar(50)); --user password
 
 CREATE TABLE Contact_List(
-	clid int serial primary key, 
-	uid int references User(uid), 
-	contact int, 
-	primary key(uid, contact));
+	clist_id int serial primary key, --contact list id
+	user_id int references User(uid), --contact list owner id
+	contact int references User(uid)); --contact id
 
 CREATE TABLE Replies (
-	original int references Message(mid), 
-	reply int references Message(mid), 
+	original int references Message(mid), --original message
+	reply int references Message(mid), --reply message
 	primary key(mid, rid));
 
 CREATE TABLE Trending (
-	sid int references Dashboard(sid), 
-	hid int references Hashtag(hid), 
+	dashid int references Dashboard(sid), --dashboard id
+	hashid int references Hashtag(hid), --hashtag id
 	primary key (sid, hid));
 
-CREATE TABLE Has_User(
-	gcid int references Group_Chat(gcid), 
-	uid int references User(uid), 
+CREATE TABLE IsAMember(
+	gcid int references Group_Chat(gcid), --group chat id
+	uid int references User(uid), --user id that belongs in group chat
 	primary key(gcid, uid));
 
-CREATE TABLE Has_Hashtag(
-	hid int references Hashtag(hid), 
-	mid int references Message(mid), 
+CREATE TABLE ContainsHashtag(
+	hid int references Hashtag(hid), --hashtag id
+	mid int references Message(mid), --message id
 	primary key(hid, mid));

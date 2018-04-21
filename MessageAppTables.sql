0add3bd640dfb566ce04829ec7a17d4c299fa138
Jahn Carlo Marrero Santiago <jahn.marrero@upr.edu>
2018-04-21 15:09:26 -0400

    updated sql file with create tables

--- MessageAppTables.sql
+++ MessageAppTables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Dashboard (
-	dash_id int serial primary key, --dashboard id
+CREATE TABLE dashboard (
+    dash_id int serial primary key, --dashboard id
   date date, --dashboard date
   total_messages int, --total messages in day
   total_replies int, --total replies in day
@@ -7,66 +7,111 @@ CREATE TABLE Dashboard (
   total_dislikes int, --total dislikes in day
   total_active_users int); --total active users in day
 
-CREATE TABLE Hashtag(
+CREATE TABLE hashtag(
 	hash_id int serial primary key, --hashtag id
 	text string, --hashtag text
 	frequency int); --hashtag frequency
 
-CREATE TABLE Message(
-	msgid int serial primary key, --message id
+insert into hashtag(hash_id, text, frequency) VALUES (1, '#DontTellToby', 5)
+insert into hashtag(hash_id, text, frequency) VALUES (2, '#DwightRegionalManager', 1)
+insert into hashtag(hash_id, text, frequency) VALUES (3, '#GoingToSandalsJamaica', 2)
+insert into hashtag(hash_id, text, frequency) VALUES (6, '#Angela&Dwight', 5)
+insert into hashtag(hash_id, text, frequency) VALUES (7, '#AssistantTOTHERegionalManager', 4)
+insert into hashtag(hash_id, text, frequency) VALUES (8, '#MichealComeBack', 6)
+
+CREATE TABLE message(
+	msg_id int serial primary key, --message id
 	text string, --message text
 	likes int, --message likes
 	dislikes int, --message dislikes
 	date date, --message date
 	time time, --message time
-	user_id int references User(uid), --user that sent the message
-	gchat_id int references Group_Chat(gcid)); --group chat where it was sent
+	person_id int references person(person_id), --user that sent the message
+	gchat_id int references group_chat(gcid)); --group chat where it was sent
 
-CREATE TABLE Group_Chat(
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES ()
+
+CREATE TABLE group_chat(
 	gchat_id int serial primary key, --group chat id
 	gchat_name varchar(12), --group chat name
-	user_id int references User(uid)); --owner of group chat
+	person_id int references person(person_id)); --owner of group chat
 
-CREATE TABLE User(
-	user_id int serial primary key, --user id
+insert into group_chat(gchat_name, person_id) VALUES ('DinnerParty', 1);
+insert into group_chat(gchat_name, person_id) VALUES ('PartyPlanning', 5);
+insert into group_chat(gchat_name, person_id) VALUES ('NOOO', 6);
+insert into group_chat(gchat_name, person_id) VALUES ('P&J', 3);
+insert into group_chat(gchat_name, person_id) VALUES ('PostMichael', 4);
+insert into group_chat(gchat_name, person_id) VALUES ('LoveMe', 8);
+
+
+CREATE TABLE person(
+	person_id int serial primary key, --user id
 	first_name varchar(50), --user first name
 	last_name varchar(50), --user last name
 	email unique string, --user email
 	phone unique char(10), --user phone
-	username unique varchar(15), --user username
 	password varchar(50)); --user password
 
-CREATE TABLE Contact_List(
+insert into person(first_name, last_name, email, phone, password) VALUES ('Michael', 'Scott', 'scott@dunder.com', '7871234567', 'boobz');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Jim', 'Halpert', 'halpert@dunder.com', '7870987654', 'cece');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Pam', 'Beesly', 'beesly@dunder.com', '7874563728', 'art');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Dwight', 'Schrute', 'schrute@dunder.com', '7870000123', 'beets');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Angela', 'Martin', 'martin@dunder.com', '7877777777', 'cats');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Toby', 'Flenderson', 'loser@dunder.com', '7876668909', 'strangle');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Kevin', 'Malone', 'malone@dunder.com', '7870696969', 'ashton');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Kelly', 'Kapoor', 'kapoor@dunder.com', '7873436574', 'ryan');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Ryan', 'Howard', 'howard@dunder.com', '7870908787', 'fraud');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Phyllis', 'Vance', 'vance@dunder.com', '7874541212', 'rainyday');
+
+
+CREATE TABLE contact_list(
 	clist_id int serial primary key, --contact list id
 	user_id int references User(uid), --contact list owner id
 	contact int references User(uid)); --contact id
 
-CREATE TABLE Replies (
-	original int references Message(mid), --original message
-	reply int references Message(mid), --reply message
-	primary key(mid, rid));
+CREATE TABLE trending (
+	dash_id int references dashboard(dash_id), --dashboard id
+	hash_id int references hashtag(hash_id), --hashtag id
+	primary key (dash_id, hash_id));
 
-CREATE TABLE Trending (
-	dashid int references Dashboard(sid), --dashboard id
-	hashid int references Hashtag(hid), --hashtag id
-	primary key (sid, hid));
+CREATE TABLE chat_members(
+    member_id serial primary key, --track how many members of chats
+	gchat_id int references group_chat(gchat_id), --group chat id
+	person_id int references person(person_id), --user id that belongs in group chat
+	primary key(gchat_id, person_id));
 
-CREATE TABLE IsAMember(
-	gcid int references Group_Chat(gcid), --group chat id
-	uid int references User(uid), --user id that belongs in group chat
-	primary key(gcid, uid));
+insert into chat_members(gchat_id, person_id) VALUES (1, 1);
+insert into chat_members(gchat_id, person_id) VALUES (1, 2);
+insert into chat_members(gchat_id, person_id) VALUES (1, 3);
+insert into chat_members(gchat_id, person_id) VALUES (1, 4);
+insert into chat_members(gchat_id, person_id) VALUES (1, 5);
+insert into chat_members(gchat_id, person_id) VALUES (1, 7);
+insert into chat_members(gchat_id, person_id) VALUES (2, 3);
+insert into chat_members(gchat_id, person_id) VALUES (2, 5);
+insert into chat_members(gchat_id, person_id) VALUES (2, 8);
+insert into chat_members(gchat_id, person_id) VALUES (2, 10);
+insert into chat_members(gchat_id, person_id) VALUES (3, 1);
+insert into chat_members(gchat_id, person_id) VALUES (3, 6);
+insert into chat_members(gchat_id, person_id) VALUES (4, 2);
+insert into chat_members(gchat_id, person_id) VALUES (4, 3);
+insert into chat_members(gchat_id, person_id) VALUES (5, 2);
+insert into chat_members(gchat_id, person_id) VALUES (5, 3);
+insert into chat_members(gchat_id, person_id) VALUES (5, 4);
+insert into chat_members(gchat_id, person_id) VALUES (5, 5);
+insert into chat_members(gchat_id, person_id) VALUES (6, 8);
+insert into chat_members(gchat_id, person_id) VALUES (6, 9);
 
-CREATE TABLE ContainsHashtag(
-	hid int references Hashtag(hid), --hashtag id
-	mid int references Message(mid), --message id
-	primary key(hid, mid));
+CREATE TABLE contains_hashtag(
+	hash_id int references hashtag(hash_id), --hashtag id
+	mid int references message(msg_id), --message id
+	primary key(hash_id, msg_id));
 
-CREATE TABLE Likes (
-  msgid int references Message(msgid), --message id
-  uid int references User(uid), --user id
-  primary key(msgid, uid));
+CREATE TABLE likes (
+  msg_id int references message(msg_id), --message id
+  person_id int references person(person_id), --user id
+  primary key(msg_id, person_id));
 
-CREATE TABLE Dislikes (
-  msgid int references Message(msgid), --message id
-  uid int references User(uid), --user id
-  primary key(msgid, uid));
+CREATE TABLE dislikes (
+  msg_id int references message(msg_id), --message id
+  person_id int references person(person_id), --user id
+  primary key(msg_id, person_id));

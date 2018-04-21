ef3c2ea86bdd81e55a33cae8c4ff8f3c1318aa6f
Jahn Carlo Marrero Santiago <jahn.marrero@upr.edu>
2018-04-21 16:02:50 -0400

    added message to sql file

--- MessageAppTables.sql
+++ MessageAppTables.sql
@@ -12,12 +12,11 @@ CREATE TABLE hashtag(
 	text string, --hashtag text
 	frequency int); --hashtag frequency
 
-insert into hashtag(hash_id, text, frequency) VALUES (1, '#DontTellToby', 5)
-insert into hashtag(hash_id, text, frequency) VALUES (2, '#DwightRegionalManager', 1)
-insert into hashtag(hash_id, text, frequency) VALUES (3, '#GoingToSandalsJamaica', 2)
-insert into hashtag(hash_id, text, frequency) VALUES (6, '#Angela&Dwight', 5)
-insert into hashtag(hash_id, text, frequency) VALUES (7, '#AssistantTOTHERegionalManager', 4)
-insert into hashtag(hash_id, text, frequency) VALUES (8, '#MichealComeBack', 6)
+insert into hashtag(hash_id, text, frequency) VALUES (1, '#DontTellToby', 5) --done
+insert into hashtag(hash_id, text, frequency) VALUES (2, '#DwightRegionalManager', 1) --done
+insert into hashtag(hash_id, text, frequency) VALUES (3, '#GoingToSandalsJamaica', 1)
+insert into hashtag(hash_id, text, frequency) VALUES (7, '#AssistantTOTHERegionalManager', 4) --done
+insert into hashtag(hash_id, text, frequency) VALUES (8, '#MichealComeBack', 2) --done
 
 CREATE TABLE message(
 	msg_id int serial primary key, --message id
@@ -29,19 +28,58 @@ CREATE TABLE message(
 	person_id int references person(person_id), --user that sent the message
 	gchat_id int references group_chat(gcid)); --group chat where it was sent
 
-insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES ()
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (1, 'Hey guys!', 0, 0, '2018-03-01', '07:30:13', 1, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (2, '#DontTellToby', 0, 0, '2018-03-01', '07:30:40', 1, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (3, 'Lets throw a dinner party!. Also Im #GoingToSandalsJamaica in summer with my boss thats also my girlfriend', 0, 0, '2018-03-01', '07:31:02', 1, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (4, 'Wow... #DontTellToby seriously Micheal?', 0, 0, '2018-03-01', '07:31:10', 2, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (5, 'Yeah I think thats a bit too much... #DontTellToby ???', 0, 0, '2018-03-01', '07:31:20', 3, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (6, 'You shouldnt be so harsh on him' , 0, 0, '2018-03-01', '07:31:30', 3, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (7, 'Yeah well whatever... he sucks', 0, 0, '2018-03-01', '07:31:50', 1, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (8, 'HAHAH MICHEAL COMEDIC GENIUS! #DontTellToby hahaahahah', 0, 0, '2018-03-01', '07:32:00', 4, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (9, 'Ill bring Mose #DontTellToby', 0, 0, '2018-03-01', '07:32:13', 4, 1)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (10, 'NO dwight... no', 0, 0, '2018-03-01', '07:32:20', 1, 1)
+
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (11, 'So what are we bringing for the party?', 0, 0, '2018-03-01', '09:17:00', 5, 2)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (12, 'Cake?', 0, 0, '2018-03-01', '09:17:30', 3, 2)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (13, 'EW NO!', 0, 0, '2018-03-01', '09:18:00', 5, 2)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (14, 'We should ask Ryan', 0, 0, '2018-03-01', '09:18:30', 8, 2)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (15, 'Hes not in the party planning comittee Kelly', 0, 0, '2018-03-01', '09:19:00', 10, 2)
+
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (16, ' Hi Micheal, I was thinking about #DwightRegionalManager... what do you', 0, 0, '2018-03-01', '09:18:30', 6, 3)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (17, 'SHUTUP TOBY. DONT TYPE ANYMORE', 0, 0, '2018-03-01', '09:18:35', 1, 3)
+
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (18, 'I love you!', 0, 0, '2018-03-01', '09:18:35', 2, 4)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (19, 'Me too <3', 0, 0, '2018-03-01', '09:18:55', 3, 4)
+
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (20, 'I miss Micheal. #MichealComeBack', 0, 0, '2018-03-01', '12:30:00', 2, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (21, 'Me too T.T #MichealComeBack', 0, 0, '2018-03-01', '12:31:00', 4, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (22, 'What does T.T mean Dwight...', 0, 0, '2018-03-01', '12:32:00', 3, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (23, 'Its a crying face, Koreans used it in the ancient wars agaisnt the japanese', 0, 0, '2018-03-01', '12:33:00', 4, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (24, 'What do you guys think of me being the Regional Manager?', 0, 0, '2018-03-01', '12:34:00', 4, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (25, 'Youll forever remain #AssistantTOTHERegionalManager', 0, 0, '2018-03-01', '12:35:00', 2, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (26, 'Yup... #AssistantTOTHERegionalManager (:', 0, 0, '2018-03-01', '12:36:00', 3, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (27, 'How dare yee create such defamatory hashtag... #AssistantTOTHERegionalManager ... Ill get you fired JIM', 0, 0, '2018-03-01', '12:37:00', 4, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (28, 'I kinda like it actually... #AssistantTOTHERegionalManager', 0, 0, '2018-03-01', '12:38:00', 5, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (29, 'Now that you say it... Me too HAha', 0, 0, '2018-03-01', '12:39:00', 4, 5)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (30, 'jesus...', 0, 0, '2018-03-01', '12:40:00', 2, 5)
+
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (31, 'Love me Ryan! I prefer to die if youre not with me...', 0, 0, '2018-03-01', '12:34:00', 8, 6)
+insert into message(msg_id, text, likes, dislikes, date, time, person_id, gchat_id) VALUES (32, 'No Kelly this is not meant to be', 0, 0, '2018-03-01', '12:35:00', 9, 6)
+
+
+
 
 CREATE TABLE group_chat(
 	gchat_id int serial primary key, --group chat id
 	gchat_name varchar(12), --group chat name
 	person_id int references person(person_id)); --owner of group chat
 
-insert into group_chat(gchat_name, person_id) VALUES ('DinnerParty', 1);
-insert into group_chat(gchat_name, person_id) VALUES ('PartyPlanning', 5);
-insert into group_chat(gchat_name, person_id) VALUES ('NOOO', 6);
-insert into group_chat(gchat_name, person_id) VALUES ('P&J', 3);
-insert into group_chat(gchat_name, person_id) VALUES ('PostMichael', 4);
-insert into group_chat(gchat_name, person_id) VALUES ('LoveMe', 8);
+insert into group_chat(gchat_name, person_id) VALUES ('DinnerParty', 1); --1
+insert into group_chat(gchat_name, person_id) VALUES ('PartyPlanning', 5); --2
+insert into group_chat(gchat_name, person_id) VALUES ('NOOO', 6); --3
+insert into group_chat(gchat_name, person_id) VALUES ('P&J', 3); --4
+insert into group_chat(gchat_name, person_id) VALUES ('PostMichael', 4); --5
+insert into group_chat(gchat_name, person_id) VALUES ('LoveMe', 8); --6
 
 
 CREATE TABLE person(
@@ -52,16 +90,16 @@ CREATE TABLE person(
 	phone unique char(10), --user phone
 	password varchar(50)); --user password
 
-insert into person(first_name, last_name, email, phone, password) VALUES ('Michael', 'Scott', 'scott@dunder.com', '7871234567', 'boobz');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Jim', 'Halpert', 'halpert@dunder.com', '7870987654', 'cece');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Pam', 'Beesly', 'beesly@dunder.com', '7874563728', 'art');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Dwight', 'Schrute', 'schrute@dunder.com', '7870000123', 'beets');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Angela', 'Martin', 'martin@dunder.com', '7877777777', 'cats');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Toby', 'Flenderson', 'loser@dunder.com', '7876668909', 'strangle');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Kevin', 'Malone', 'malone@dunder.com', '7870696969', 'ashton');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Kelly', 'Kapoor', 'kapoor@dunder.com', '7873436574', 'ryan');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Ryan', 'Howard', 'howard@dunder.com', '7870908787', 'fraud');
-insert into person(first_name, last_name, email, phone, password) VALUES ('Phyllis', 'Vance', 'vance@dunder.com', '7874541212', 'rainyday');
+insert into person(first_name, last_name, email, phone, password) VALUES ('Michael', 'Scott', 'scott@dunder.com', '7871234567', 'boobz');  --1
+insert into person(first_name, last_name, email, phone, password) VALUES ('Jim', 'Halpert', 'halpert@dunder.com', '7870987654', 'cece'); --2
+insert into person(first_name, last_name, email, phone, password) VALUES ('Pam', 'Beesly', 'beesly@dunder.com', '7874563728', 'art'); --3
+insert into person(first_name, last_name, email, phone, password) VALUES ('Dwight', 'Schrute', 'schrute@dunder.com', '7870000123', 'beets'); --4
+insert into person(first_name, last_name, email, phone, password) VALUES ('Angela', 'Martin', 'martin@dunder.com', '7877777777', 'cats'); --5
+insert into person(first_name, last_name, email, phone, password) VALUES ('Toby', 'Flenderson', 'loser@dunder.com', '7876668909', 'strangle'); --6
+insert into person(first_name, last_name, email, phone, password) VALUES ('Kevin', 'Malone', 'malone@dunder.com', '7870696969', 'ashton'); --7
+insert into person(first_name, last_name, email, phone, password) VALUES ('Kelly', 'Kapoor', 'kapoor@dunder.com', '7873436574', 'ryan'); --8
+insert into person(first_name, last_name, email, phone, password) VALUES ('Ryan', 'Howard', 'howard@dunder.com', '7870908787', 'fraud'); --9
+insert into person(first_name, last_name, email, phone, password) VALUES ('Phyllis', 'Vance', 'vance@dunder.com', '7874541212', 'rainyday'); --10
 
 
 CREATE TABLE contact_list(
